b8f820cb262722fdb77e984a1ce16dee675273f0
zuehan0 <lololll0o0808@gmail.com>
2025-06-12 14:41:14 +0900

    refactor: restructure RAG chain into modular retriever, prompt, and QA flow (retriever/프롬프트/응답 체인 분리 및 구조 개선)

--- chat.py
+++ chat.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from llm import get_ai_message
+from llm import stream_ai_message
 
 
 st.set_page_config(page_title='전세사기피해 상담 챗봇', page_icon='📢')
@@ -27,7 +27,7 @@ if user_question := st.chat_input(placeholder=placeholder): #prompt 창(채팅�
         # ai_msg = get_ai_message(user_question)
 
         session_id = 'user-session'
-        ai_msg = get_ai_message(user_question, session_id=session_id)
+        ai_msg = stream_ai_message(user_question, session_id=session_id)
 
         with st.chat_message('ai'):
             #AI msg 화면 출력
--- llm.py
+++ llm.py
@@ -1,21 +1,20 @@
 import os
 
 from dotenv import load_dotenv
+from langchain.chains import (create_history_aware_retriever, create_retrieval_chain)
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
-from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables import RunnableLambda
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
 
-
 ## 환경변수 읽어오기 =====================================================
 load_dotenv()
 
-## llm 함수 정의 =========================================================
+## llm 생성 =========================================================
 def get_llm(model='gpt-4o'):
     llm = ChatOpenAI(model=model)
     return llm
@@ -38,7 +37,7 @@ def get_database():
     return database
 
 
-## Statefully manage chat history ========================================
+## [세션 별 hisotyr] 저장 ========================================
 store = {}
 
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
@@ -46,29 +45,18 @@ def get_session_history(session_id: str) -> BaseChatMessageHistory:
         store[session_id] = ChatMessageHistory()
     return store[session_id]
 
-
-## retrievalQA 함수 정의 =================================================
-def get_retrievalQA():
-    LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
-
-    ## LLM 모델 지정
-    llm = get_llm()
-
-    ## vector store에서 index 정보
-    database = get_database()
-    retriever = database.as_retriever(search_kwargs={'k': 2})
-
-    ## 코드 추가 ######################################################
-    from langchain.chains import create_history_aware_retriever
-    from langchain_core.prompts import MessagesPlaceholder
-
+# 히스토리 기반 리트리버
+def get_history_retriever(llm, retriever):
     contextualize_q_system_prompt = (
-        "Given a chat history and the latest user question "
-        "which might reference context in the chat history, "
-        "formulate a standalone question which can be understood "
-        "without the chat history. Do NOT answer the question, "
-        "just reformulate it if needed and otherwise return it as is."
-    )
+        '''
+- 너는 대화형 Q&A 시스템의 고도화된 질문 전처리 담당이야.
+- 사용자의 질문과 제공된 (chat history)을 참고해서 검색에 가장 적합한 문장으로 사용자의 질문을 명확하게 수정해.
+- 'chat history'를 참고해서 현재 질문에 생략된 주어, 목적어 등 맥락상 필요한 정보를 보충하여 질문을 완성해.
+- 특히 이전대화에서 언급된 키워드나 명사가 있다면 현재 질문에 자연스럽게 통합시켜.
+- 만약 질문이 이미 명확하면 그대로 반환해.
+- 질문에 대해 절대 답변하지 말고, 단지 "이해 가능한 독립 질문"으로 고치기만 해.
+'''
+    )   
 
     contextualize_q_prompt = ChatPromptTemplate.from_messages(
         [
@@ -81,9 +69,12 @@ def get_retrievalQA():
     history_aware_retriever = create_history_aware_retriever(
         llm, retriever, contextualize_q_prompt
     )
-    ###################################################################
+    return history_aware_retriever
 
-    ### Answer question ###
+
+
+def get_prompt():
+       ### Answer question ###
     system_prompt = (
     '''[identity]
 - 당신은 전세사기피해 법률 전문가입니다.
@@ -104,9 +95,25 @@ def get_retrievalQA():
             ("human", "{input}"),
         ]
     )
+    return qa_prompt
 
-    from langchain.chains import create_retrieval_chain
-    from langchain.chains.combine_documents import create_stuff_documents_chain
+## 전체 체인 구성 =================================================
+def build_conversational_chain():
+    LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
+
+    ## LLM 모델 지정
+    llm = get_llm()
+
+    ## vector store에서 index 정보
+    database = get_database()
+    retriever = database.as_retriever(search_kwargs={'k': 2})
+
+    ## 히스토리 기반 리트리버 생성
+    history_aware_retriever = get_history_retriever(llm, retriever)
+
+    ## QA 프롬프트 생성
+    qa_prompt = get_prompt()
+
     question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
     rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
 
@@ -122,8 +129,8 @@ def get_retrievalQA():
 
 
 ## [AI Message 함수 정의] ================================================
-def get_ai_message(user_message, session_id='default'):
-    qa_chain = get_retrievalQA()
+def stream_ai_message(user_message, session_id='default'):
+    qa_chain = build_conversational_chain()
 
     ai_message = qa_chain.stream(
         {'input': user_message},
